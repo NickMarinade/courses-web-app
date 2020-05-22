2d99ec9081410bd2762998c3989f265778db194f
Mykola Marenich <marenich.mykola@gmail.com>
2020-05-23 00:12:24 +0200

    backend done

--- api/routes.js
+++ api/routes.js
@@ -3,9 +3,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', controllers.hello);
-
+router.get('/', (req, res) => {
+    res.json({ api: 'courses!' });
+});
 // write your routes
+router.get('/db', controllers.getCourses);
+router.post('/db', controllers.postCourse);
+router.put('/db/:id', controllers.updCourse);
+router.delete('/db/:id', controllers.deleteCourse);
 
 
 module.exports = router;
